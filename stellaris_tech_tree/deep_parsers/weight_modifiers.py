193935f6a4c5789631fdf40d8612425b983cb408
Cother <ihamsterball@gmail.com>
2017-09-19 19:26:23 +0800

    Improved internationalization translation in techs

--- stellaris_tech_tree/deep_parsers/weight_modifiers.py
+++ stellaris_tech_tree/deep_parsers/weight_modifiers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from pprint import pprint
+from django.utils.translation import ugettext
 import re
 import yaml
 import sys
@@ -44,6 +44,12 @@ def _parse_condition(condition):
     value = condition[key]
     return globals()['_localize_' + key.lower()](value)
 
+def _expert(expert):
+    return {
+        'Physics': ugettext('Physics'),
+        'Society': ugettext('Society'),
+        'Engineering': ugettext('Engineering'),
+    }.get(expert, 'Shouldn\'t be here')
 
 def _localize_factor(factor):
     return u'\xD7{}'.format(factor)
@@ -56,48 +62,48 @@ def _localize_add(add):
 
 def _localize_has_ethic(value):
     ethic = localization_map[value]
-    return 'Has {} Ethic'.format(ethic)
+    return ugettext('Has {} Ethic').format(ethic)
 
 def _localize_has_not_ethic(value):
     ethic = localization_map[value]
-    return 'Does NOT have {} Ethic'.format(ethic)
+    return ugettext('Does NOT have {} Ethic').format(ethic)
 
 def _localize_has_civic(value):
     civic = localization_map[value]
-    return 'Has {} Government Civic'.format(civic)
+    return ugettext('Has {} Government Civic').format(civic)
 
 def _localize_has_valid_civic(value):
     civic = localization_map[value]
-    return 'Has {} Goverment Civic'.format(civic)
+    return ugettext('Has {} Government Civic').format(civic)
 
 def _localize_has_ascension_perk(value):
     perk = localization_map[value]
-    return 'Has {} Ascension Perk'.format(perk)
+    return ugettext('Has {} Ascension Perk').format(perk)
 
 def _localize_has_megastructure(value):
     megastructure = localization_map[value]
-    return 'Has Megatructure {}'.format(megastructure)
+    return ugettext('Has Megatructure {}').format(megastructure)
 
 def _localize_has_policy_flag(value):
     policy_flag = localization_map[value]
-    return 'Has {} Policy'.format(policy_flag)
+    return ugettext('Has {} Policy').format(policy_flag)
 
 
 def _localize_has_trait(value):
     trait = localization_map[value]
-    return 'Has {} Trait'.format(trait)
+    return ugettext('Has {} Trait').format(trait)
 
 def _localize_has_authority(value):
     authority = localization_map[value]
-    return 'Has {} Authority'.format(authority)
+    return ugettext('Has {} Authority').format(authority)
 
 def _localize_host_has_dlc(dlc):
     # dlc = localization_map[value]
-    return 'Host does has the {} DLC'.format(dlc)
+    return ugettext('Host does has the {} DLC').format(dlc)
 
 def _localize_host_has_not_dlc(dlc):
     # dlc = localization_map[value]
-    return 'Host does NOT have the {} DLC'.format(dlc)
+    return ugettext('Host does NOT have the {} DLC').format(dlc)
 
 def _localize_has_technology(value):
     try:
@@ -105,7 +111,7 @@ def _localize_has_technology(value):
     except KeyError:
         technology = value
 
-    return 'Has {} Technology'.format(technology)
+    return ugettext('Has {} Technology').format(technology)
 
 
 def _localize_has_not_technology(value):
@@ -114,77 +120,77 @@ def _localize_has_not_technology(value):
     except KeyError:
         technology = value
 
-    return 'Does NOT have {} Technology'.format(technology)
+    return ugettext('Does NOT have {} Technology').format(technology)
 
 
 def _localize_has_modifier(value):
     modifier = localization_map[value]
-    return 'Has the {} modifier'.format(modifier)
+    return ugettext('Has the {} modifier').format(modifier)
 
 
 def _localize_has_not_modifier(value):
     modifier = localization_map[value]
-    return 'Does NOT have the {} modifier'.format(modifier)
+    return ugettext('Does NOT have the {} modifier').format(modifier)
 
 
 def _localize_is_country_type(value):
-    return 'Is of the {} country type'.format(value)
+    return ugettext('Is of the {} country type').format(value)
 
 
 def _localize_ideal_planet_class(value):
-    return 'Is ideal class'.format(value)
+    return ugettext('Is ideal class').format(value)
 
 
 def _localize_is_planet_class(value):
     planet_class = localization_map[value]
-    return 'Is {}'.format(planet_class)
+    return ugettext('Is {}').format(planet_class)
 
 
 def _localize_has_government(value):
     government = localization_map[value]
-    return 'Has {}'.format(government)
+    return ugettext('Has {}').format(government)
 
 
 def _localize_is_colony(value):
-    return 'Is a Colony' if value == 'yes' \
-        else 'Is NOT a Colony'
+    return ugettext('Is a Colony') if value == 'yes' \
+        else ugettext('Is NOT a Colony')
 
 
 def _localize_allows_slavery(value):
-    return 'Allows Slavery' if value == 'yes' \
-        else 'Does NOT allow Slavery'
+    return ugettext('Allows Slavery') if value == 'yes' \
+        else ugettext('Does NOT allow Slavery')
 
 
 def _localize_has_federation(value):
-    return 'Is in a Federation' if value == 'yes' \
-        else 'Is NOT in a Federation'
+    return ugettext('Is in a Federation') if value == 'yes' \
+        else ugettext('Is NOT in a Federation')
 
 def _localize_num_owned_planets(value):
     operator, value = _operator_and_value(value)
-    return 'Number of owned planets is {} {}'.format(operator, value)
+    return ugettext('Number of owned planets is {} {}').format(operator, value)
 
 def _localize_count_owned_pops(value):
     operator, value = _operator_and_value(value[1]['count'])
-    return 'Number of enslaved planets {} {}'.format(operator, value)
+    return ugettext('Number of enslaved planets {} {}').format(operator, value)
 
 def _localize_num_communications(value):
     operator, value = _operator_and_value(value)
-    return 'Number of owned planets is {} {}'.format(operator, value)
+    return ugettext('Number of owned planets is {} {}').format(operator, value)
 
 
 def _localize_has_communications(value):
-    return 'Has communications with your Empire'
+    return ugettext('Has communications with your Empire')
 
 
 def _localize_is_ai(value):
-    return 'Is AI controlled' if value == 'yes' else 'Is NOT AI controlled'
+    return ugettext('Is AI controlled') if value == 'yes' else ugettext('Is NOT AI controlled')
 
 
 def _localize_is_same_species(value):
     localized_value = 'Dominant' \
                       if value.lower() == 'root' \
                          else localization_map[value]
-    return 'Is of the {} Species'.format(localized_value)
+    return ugettext('Is of the {} Species').format(localized_value)
 
 
 def _localize_is_species(value):
@@ -193,42 +199,42 @@ def _localize_is_species(value):
                          else localization_map[value]
     article = 'an' if localized_value[0].lower() in 'aeiou' else 'a'
 
-    return 'Is {} {}'.format(article, localized_value)
+    return ugettext('Is {} {}').format(article, localized_value)
 
 
 def _localize_is_species_class(value):
     localized_value = localization_map[value]
     article = 'an' if localized_value[0].lower() in 'aeiou' else 'a'
 
-    return 'Is {} {}'.format(article, localized_value)
+    return ugettext('Is {} {}').format(article, localized_value)
 
 
 def _localize_is_enslaved(value):
-        return 'Pop is enslaved' if value == 'yes' else 'Pop is NOT enslaved'
+        return ugettext('Pop is enslaved') if value == 'yes' else ugettext('Pop is NOT enslaved')
 
 
 def _localize_years_passed(value):
     operator, value = _operator_and_value(value)
-    return 'Number of years since game start is {} {}'.format(operator, value)
+    return ugettext('Number of years since game start is {} {}').format(operator, value)
 
 
 def _localize_not_years_passed(value):
     operator, value = _operator_and_value(value)
-    return 'Number of years since game start is NOT {} {}'.format(operator, value)
+    return ugettext('Number of years since game start is NOT {} {}').format(operator, value)
 
 
 def _localize_has_country_flag(value):
-    return 'Has {} country flag'.format(value)
+    return ugettext('Has {} country flag').format(value)
 
 
 def _localize_has_not_country_flag(value):
-    return 'Does NOT have {} country flag'.format(value)
+    return ugettext('Does NOT have {} country flag').format(value)
 
 
 def _localize_research_leader(values, negated=False):
-    leader = 'Research Leader ({})'.format(values[0]['area'].title())
+    leader = ugettext('Research Leader ({})').format(_expert(values[0]['area'].title()))
     if negated:
-        leader = 'NOT ' + leader
+        leader = ugettext('NOT ') + leader
 
     localized_conditions = []
     for condition in values[1:]:
@@ -249,74 +255,73 @@ def _localize_not_research_leader(values):
 
 def _localize_has_level(value):
     operator, level = _operator_and_value(value)
-    return 'Skill level is {} {}'.format(operator, level)
-
+    return ugettext('Skill level is {} {}').format(operator, level)
 
 def _localize_has_expertise(value):
     expertise = localization_map[value]
     if expertise.startswith('Expertise'):
         truncated = expertise.replace('Expertise: ', '')
-        condition = 'Is {} Expert'.format(truncated)
+        condition = ugettext('Is {} Expert').format(truncated)
     else:
-        condition = 'Is {}'.format(expertise)
+        condition = ugettext('Is {}').format(expertise)
 
     return condition
 
 
 def _localize_any_system_within_border(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'Any System within Borders': parsed_values}
+    return {ugettext('Any System within Borders'): parsed_values}
 
 
 def _localize_is_in_cluster(value):
-    return 'Is in a {} Cluster'.format(value)
+    return ugettext('Is in a {} Cluster').format(value)
 
 
 def _localize_any_country(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'Any Country': parsed_values}
+    return {ugettext('Any Country'): parsed_values}
 
 def _localize_any_relation(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'Any Relation': parsed_values}
+    return {ugettext('Any Relation'): parsed_values}
 
 def _localize_any_owned_pop(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'Any empire Pop': parsed_values}
+    return {ugettext('Any empire Pop'): parsed_values}
 
 def _localize_not_any_owned_pop(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'NOT any owned Pop': parsed_values}
+    return {ugettext('NOT any owned Pop'): parsed_values}
 
 
 def _localize_any_owned_planet(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'Any owned Planet': parsed_values}
+    return {ugettext('Any owned Planet'): parsed_values}
 
 
 def _localize_any_planet(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'Any Planet': parsed_values}
+    return {ugettext('Any Planet'): parsed_values}
 
 
 def _localize_not_any_owned_planet(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'NOT any owned Planet': parsed_values}
+    return {ugettext('NOT any owned Planet'): parsed_values}
 
 
 def _localize_any_tile(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'Any Tile': parsed_values}
+    return {ugettext('Any Tile'): parsed_values}
 
 
 def _localize_has_blocker(value):
     blocker = localization_map[value]
-    return 'Has {} Tile Blocker'.format(blocker)
+    return ugettext('Has {} Tile Blocker').format(blocker)
 
 
 def _localize_any_neighbor_country(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'Any Neighbor Country': parsed_values}
+    return {ugettext('Any Neighbor Country'): parsed_values}
 
 
 def _localize_has_resource(value):
@@ -324,28 +329,28 @@ def _localize_has_resource(value):
     operator, amount = _operator_and_value(amount)
     localized_resource = localization_map[resource]
 
-    return 'Has {} {} {}'.format(operator, amount, localized_resource)
+    return ugettext('Has {} {} {}').format(operator, amount, localized_resource)
 
 def _localize_has_any_megastructure_in_empire(value):
-    return 'Has any megastructure in empire'
+    return ugettext('Has any megastructure in empire')
 
 def _localize_always(value):
-    return 'Always' if value == 'yes' else 'Never'
+    return ugettext('Always') if value == 'yes' else ugettext('Never')
 
 
 def _localize_and(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'All of the following': parsed_values}
+    return {ugettext('All of the following'): parsed_values}
 
 
 def _localize_or(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'Any of the following': parsed_values}
+    return {ugettext('Any of the following'): parsed_values}
 
 
 def _localize_nor(values):
     parsed_values = [_parse_condition(value) for value in values]
-    return {'None of the following': parsed_values}
+    return {ugettext('None of the following'): parsed_values}
 
 
 def _localize_not(value):
@@ -366,12 +371,12 @@ def _localize_not(value):
 
 def _operator_and_value(data):
     if type(data) is int:
-        operator = 'equal to'
+        operator = ugettext('equal to')
         value = data
     elif type(data) is dict:
         operator = {
-            '>': 'greater than',
-            '<': 'less than'
+            '>': ugettext('greater than'),
+            '<': ugettext('less than')
         }[list(data)[0]]
         value = next(iter(data.values()))
 
