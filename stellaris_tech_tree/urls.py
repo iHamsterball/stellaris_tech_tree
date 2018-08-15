c3d4d3ac5178fbbe641a0304cefe85383f4d260e
Cother <ihamsterball@gmail.com>
2018-08-15 22:44:31 +0800

    Remove deprecated function

--- stellaris_tech_tree/urls.py
+++ stellaris_tech_tree/urls.py
@@ -17,16 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
-from django.views.i18n import javascript_catalog
+from django.views.i18n import JavaScriptCatalog
 from django.conf.urls import include
 from django.conf import settings
 from . import views
 from . import interfaces
-
-js_info_dict = {
-    'domain': 'djangojs',
-    'packages': ('jscript_i18n',),
-}
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -35,7 +30,7 @@ urlpatterns = [
     url(r'^about/', views.about),
     url(r'^techs/', interfaces.techs),
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^jsi18n/$', javascript_catalog, js_info_dict, name='javascript-catalog'),
+    url(r'^jsi18n/$', JavaScriptCatalog.as_view(), name='javascript-catalog'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
