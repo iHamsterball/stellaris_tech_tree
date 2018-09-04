92ad270e0e499b696238ed78151dc14bc0b5d7bd
Cother <ihamsterball@gmail.com>
2018-09-04 17:38:52 +0800

    Optimize drag function to increase fps

--- stellar_static/js/drag.js
+++ stellar_static/js/drag.js
@@ -13,8 +13,8 @@ function drag(elementId) {
   let element = document.getElementById(elementId);
   minX = -element.scrollWidth + window.innerWidth;
   minY = -element.scrollHeight + window.innerHeight;
-  maxX = 0;
-  maxY = 0;
+  maxX = "0";
+  maxY = "0";
   element.style.position = 'absolute';
   element.style.top = '58px';//初态的位置
   element.style.left = '0px';
@@ -34,38 +34,7 @@ function drag(elementId) {
     37: "LEFT"
   };
 
-  document.body.onkeydown = function (e) {
-    //屏蔽默认按键功能
-    //e.preventDefault();
-    if (direction[e.keyCode]) {
-      key[direction[e.keyCode]] = true;
-      console.log(direction[e.keyCode]);
-      keyboardMove();
-    }
-  }
-
-  document.body.onkeyup = function (e) {
-    if (direction[e.keyCode]) {
-      key[direction[e.keyCode]] = false;
-      console.log(direction[e.keyCode]);
-    }
-  }
-
-  function keyboardMove() {
-    let offsetX = 0, offsetY = 0;
-    if (key['UP']) {
-      offsetY -= 20;
-    }
-    if (key['DOWN']) {
-      offsetY += 20;
-    }
-    if (key['LEFT']) {
-      offsetX -= 20;
-    }
-    if (key['RIGHT']) {
-      offsetX += 20;
-    }
-    element.style.position = 'absolute';
+  function getTransformValue(element) {
     let cst = window.getComputedStyle(element);
     let curTransform = cst.webkitTransform ||
                        cst.mozTransform ||
@@ -80,38 +49,87 @@ function drag(elementId) {
       values = values.split(')')[0],
       values = values.split(',');
     }
+    return values;
+  }
+
+  function checkBorder(translateX, translateY) {
+    let retX, retY;
+    if (typeof(translateX) != "undefined") {
+      retX = translateX;
+      translateX < minX && (retX = minX);
+      translateX > maxX && (retX = maxX);
+    }
+    if (typeof(translateY) != "undefined") {
+      retY = translateY;
+      translateY < minY && (retY = minY);
+      translateY > maxY && (retY = maxY);
+    }
+    return retX || retY;
+  }
+
+  function keyboardMove() {
+    if (pendingKeyboardUpdate) cancelAnimationFrame(pendingKeyboardUpdate);
+    let offsetX = 0, offsetY = 0;
+    if (key['UP']) {
+      offsetY -= 15;
+    }
+    if (key['DOWN']) {
+      offsetY += 15;
+    }
+    if (key['LEFT']) {
+      offsetX -= 15;
+    }
+    if (key['RIGHT']) {
+      offsetX += 15;
+    }
+    element.style.position = 'absolute';
+    let values = getTransformValue(element);
     // values[0:5]: matrix(cos(a), sin(a), -sin(a), cos(a), x, y)
-    tempY = parseInt(values[5]) - offsetY;
     tempX = parseInt(values[4]) - offsetX;
+    tempY = parseInt(values[5]) - offsetY;
     if (isNaN(tempY))
       tempY = maxY;
     if (isNaN(tempX))
       tempX = maxX;
 
     console.log(tempX, tempY);
-    if (tempY < minY) {
-      tempY = minY;
-    }
-    if (tempY > maxY) {
-      tempY = maxY;
-    }
-    if (tempX < minX) {
-      tempX = minX;
-    }
-    if (tempX > maxX) {
-      tempX = maxX;
-    }
+    tempX = checkBorder(tempX, undefined);
+    tempY = checkBorder(undefined, tempY);
     element.style.transform = 'translate(' + tempX + 'px, ' + tempY + 'px)';
+
+    if (krAF) {
+      pendingKeyboardUpdate = requestAnimationFrame(keyboardMove);
+    }
   }
 
+  let krAF = true, pendingKeyboardUpdate = null;
+  document.body.onkeydown = function (e) {
+    //屏蔽默认按键功能
+    //e.preventDefault();
+    if (direction[e.keyCode]) {
+      key[direction[e.keyCode]] = true;
+      krAF = true;
+      console.log(direction[e.keyCode]);
+      pendingKeyboardUpdate = requestAnimationFrame(keyboardMove);
+    }
+  }
+
+  document.body.onkeyup = function (e) {
+    if (direction[e.keyCode]) {
+      key[direction[e.keyCode]] = false;
+      console.log(direction[e.keyCode]);
+      if (key['UP']||key['DOWN']||key['LEFT']||key['RIGHT']) krAF = false;
+    }
+  }
 
   let position = {
     offsetX: 0, //点击处偏移元素的X
     offsetY: 0, //偏移Y值
     endX: 0, //移动过程中的指针/触屏位置X
     endY: 0, //位置Y
-    state: 0 //是否正处于拖拽状态，1表示正在拖拽，0表示释放
-  };
+    state: false, //是否正处于拖拽状态，true表示正在拖拽，false表示释放
+    rAF: true
+  }, pendingUpdate = null;
 
   //获得兼容的event对象
   function getEvent(event) {
@@ -120,40 +138,18 @@ function drag(elementId) {
 
   //移动元素
   function setLocation() {
+    position.rAF = true;
     //设置绝对位置在文档中，鼠标当前位置-开始拖拽时的偏移位置
-    if (position.state == 1) {
-      element.style.position = 'absolute';
-      let cst = window.getComputedStyle(element);
-      let curTransform = cst.webkitTransform ||
-                         cst.mozTransform ||
-                         cst.msTransform ||
-                         cst.oTransform ||
-                         "err";// No transform set("none") or browser not supported
-      let values;
-      if (curTransform == "none") {
-        values = [1, 0, 0, 1, 0, 0];
-      } else {
-        values = curTransform.split('(')[1],
-        values = values.split(')')[0],
-        values = values.split(',');
-      }
-      // values[0:5]: matrix(cos(a), sin(a), -sin(a), cos(a), x, y)
-      translateX = parseInt(values[4]) + (position.endX - position.offsetX);
-      translateY = parseInt(values[5]) + (position.endY - position.offsetY);
-      if (translateY < minY) {
-        translateY = minY;
-      }
-      if (translateY > maxY) {
-        translateY = maxY;
-      }
-      if (translateX < minX) {
-        translateX = minX;
-      }
-      if (translateX > maxX) {
-        translateX = maxX;
-      }
-      element.style.transform = "translate(" + translateX + "px," + translateY + "px)";
-    }
+    element.style.position = 'absolute';
+    let values = getTransformValue(element);
+    // values[0:5]: matrix(cos(a), sin(a), -sin(a), cos(a), x, y)
+    translateX = parseInt(values[4]) + (position.endX - position.offsetX);
+    translateY = parseInt(values[5]) + (position.endY - position.offsetY);
+    position.offsetX = position.endX;
+    position.offsetY = position.endY;
+    translateX = checkBorder(translateX, undefined);
+    translateY = checkBorder(undefined, translateY);
+    element.style.transform = "translate(" + translateX + "px," + translateY + "px)";
   }
 
   //绑定鼠标按下动作
@@ -162,7 +158,7 @@ function drag(elementId) {
     let e = getEvent(event);
     position.offsetX = e.clientX;
     position.offsetY = e.clientY;
-    position.state = 1;
+    position.state = true;
   }
 
   //绑定触屏开始动作
@@ -173,30 +169,30 @@ function drag(elementId) {
     //只用第一个触控点（暂时）
     position.offsetX = e.touches.item(0).clientX;
     position.offsetY = e.touches.item(0).clientY;
-    position.state = 1;
+    position.state = true;
   }
 
   //鼠标拖动元素动作
   function onMousemove(event) {
     let e = getEvent(event);
-    if (position.state) {
-      position.endX = e.clientX;
-      position.endY = e.clientY;
-      setLocation();
-      position.offsetX = position.endX;
-      position.offsetY = position.endY;
+    position.endX = e.clientX;
+    position.endY = e.clientY;
+    if (position.state && position.rAF) {
+      position.rAF = false;
+      pendingUpdate = requestAnimationFrame(setLocation);
+      //setLocation();
     }
   }
 
   //触屏拖动元素动作
   function onTouchmove(event) {
     let e = getEvent(event);
-    if (position.state) {
-      position.endX = e.touches.item(0).clientX;
-      position.endY = e.touches.item(0).clientY;
-      setLocation();
-      position.offsetX = position.endX;
-      position.offsetY = position.endY;
+    position.endX = e.touches.item(0).clientX;
+    position.endY = e.touches.item(0).clientY;
+    if (position.state && position.rAF) {
+      position.rAF = false;
+      requestAnimationFrame(setLocation);
+      //setLocation();
     }
   }
 
@@ -234,13 +230,15 @@ function drag(elementId) {
   //释放拖拽状态
   element.addEventListener('mouseup', function (event) {
     element.style.cursor = "auto";
-    position.state = 0;
+    position.state = false;
+    position.rAF = true;
+    cancelAnimationFrame(pendingUpdate);
   }, false);
 
   //触屏释放状态
   element.addEventListener('touchend', function (event) {
     element.style.cursor = "auto"
-    position.state = 0;
+    position.state = false;
     //真的太脏了(#2
     if (event.target.className == 'description'
     || event.target.className == 'weight-modifiers'
